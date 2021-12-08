524716d4004bf6c347ae8f725f5a45d6a88a6787
Thiago Túlio Brandão Silva <47676524+ttbs1@users.noreply.github.com>
2021-12-08 01:00:43 -0300

    using input to create products

--- trackmobApp/src/pages/Create/index.js
+++ trackmobApp/src/pages/Create/index.js
@@ -1,18 +1,57 @@
 import React from 'react';
-import {View, Text} from 'react-native';
-import {useRoute} from '@react-navigation/native';
+import { View, Text, TextInput, StyleSheet, TouchableOpacity } from 'react-native';
+import { useRoute } from '@react-navigation/native';
 
-export default function Create({navigation}) {
-  const route = useRoute();
+class Create extends React.Component {
 
-  return (
-    <View>
-      <Text>
-        {route.params.callback({
-          nome: 'produto ruim',
-          valor: 'caro',
-        })}
-      </Text>
-    </View>
-  );
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: "",
+            price: "",
+            category: "",
+            tags: []
+        }
+    }
+
+
+    render() {
+        const { navigation } = this.props;
+        const { route } = this.props;
+
+        return (
+            <View>
+                <TextInput style={styles.input} onChangeText={text => { this.setState({ name: text }) }} value={this.state.name} />
+                <Text>
+                    {this.state.name}
+                </Text>
+                <TouchableOpacity
+                    style={styles.btn}
+                    onPress={() => { 
+                        route.params.callback({
+                            nome: this.state.name,
+                            valor: "1 real"
+                        })
+                    }}>
+                    <Text>+ Salvar</Text>
+                </TouchableOpacity>
+            </View>
+        )
+    }
 }
+
+const styles = StyleSheet.create({
+    input: {
+        borderWidth: 1
+    },
+    btn: {
+        backgroundColor: '#aaa',
+        width: 100,
+        padding: 10,
+        borderColor: 'black',
+        borderWidth: 1,
+        borderRadius: 5,
+    }
+});
+
+export default Create;
--- trackmobApp/src/pages/Home/index.js
+++ trackmobApp/src/pages/Home/index.js
@@ -1,45 +1,69 @@
-import React, {useState, useEffect} from 'react';
-import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
-export default function Home({navigation}) {
-  const [produtos, setProdutos] = useState();
+class Home extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            products: false
+        }
 
-  const handleClick = produto => {
-    setProdutos(() => [produtos, produto]);
-  };
+        this.handleClick = this.handleClick.bind(this);
+    }
 
-  useEffect(() => {
-    setProdutos(produtos);
-  }, [produtos]);
+    handleClick = product => {
+        let products = this.state.products ? [...this.state.products, product] : new Array(product);
+        this.setState({ products });
+    };
 
-  return (
-    <View style={styles.container}>
-      <View>
-        <TouchableOpacity
-          style={styles.btn}
-          onPress={() =>
-            navigation.navigate('Create', {callback: handleClick})
-          }>
-          <Text>+ Cadastrar</Text>
-        </TouchableOpacity>
-        <View>
-          <Text>{console.log(produtos)}</Text>
-        </View>
-      </View>
-    </View>
-  );
+    render() {
+        const { navigation } = this.props;
+
+        return (
+            <View style={styles.container}>
+                <View>
+                    <TouchableOpacity
+                        style={styles.btn}
+                        onPress={() =>
+                            navigation.navigate('Create', { callback: this.handleClick })
+                        }>
+                        <Text>+ Cadastrar</Text>
+                    </TouchableOpacity>
+                    <View>
+                        
+                        {
+                            this.state.products
+                                ? this.state.products.map(response => {
+                                    return (
+                                        <View style={styles.containerProd}>
+                                            <Text>{response.nome}</Text>
+                                            <Text>{response.valor}</Text>
+                                            <Text>{response.categoria}</Text>
+                                            <Text>Tags</Text>
+                                        </View>
+                                    )
+                                })
+                                : null
+                        }
+                    </View>
+                </View>
+            </View>
+        )
+    };
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  btn: {
-    backgroundColor: '#aaa',
-    width: 100,
-    padding: 10,
-    borderColor: 'black',
-    borderWidth: 1,
-    borderRadius: 5,
-  },
+    container: {
+        flex: 1,
+    },
+    btn: {
+        backgroundColor: '#aaa',
+        width: 100,
+        padding: 10,
+        borderColor: 'black',
+        borderWidth: 1,
+        borderRadius: 5,
+    },
 });
+
+export default Home;
