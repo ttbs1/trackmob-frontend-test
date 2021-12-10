c6311cd4e33570b765c62b6c7efd45170c478e80
Thiago Túlio Brandão Silva <47676524+ttbs1@users.noreply.github.com>
2021-12-10 19:20:42 -0300

    visual updates

--- trackmobApp/src/pages/Create/index.js
+++ trackmobApp/src/pages/Create/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Text, TextInput, StyleSheet, TouchableOpacity } from 'react-native';
 import { useRoute } from '@react-navigation/native';
+import { Picker } from '@react-native-picker/picker';
 
 class Create extends React.Component {
 
@@ -9,7 +10,7 @@ class Create extends React.Component {
         this.state = {
             name: "",
             price: "",
-            category: "",
+            category: "category1",
             tags: []
         }
     }
@@ -20,29 +21,63 @@ class Create extends React.Component {
         const { route } = this.props;
 
         return (
-            <View>
-                <TextInput style={styles.input} onChangeText={text => { this.setState({ name: text }) }} value={this.state.name} />
-                <Text>
-                    {this.state.name}
-                </Text>
-                <TouchableOpacity
-                    style={styles.btn}
-                    onPress={() => { 
-                        route.params.callback({
-                            nome: this.state.name,
-                            valor: "1 real"
-                        })
-                    }}>
-                    <Text>+ Salvar</Text>
-                </TouchableOpacity>
-            </View>
+            <>
+                <View style={styles.header}>
+                    <Text style={styles.title}>Trackmob Frontend Test</Text>
+                    <Text>My Products</Text>
+                </View>
+                <View style={styles.container}>
+                    <TextInput style={styles.input} onChangeText={text => { this.setState({ name: text }) }} value={this.state.name} placeholder="Product name" />
+
+                    <View style={styles.input}>
+                        <Picker
+                            selectedValue={this.state.category}
+                            onValueChange={(itemValue) =>
+                                this.setState({ category: itemValue })
+                            }>
+                            <Picker.Item label="Category 1" value="category1" />
+                            <Picker.Item label="Category 2" value="category2" />
+                            <Picker.Item label="Category 3" value="category3" />
+                        </Picker>
+                    </View>
+
+                    <TextInput style={styles.input} onChangeText={text => { this.setState({ price: text }) }} value={this.state.price} placeholder="100.00" keyboardType="numeric" />
+                    <TextInput style={styles.input} onChangeText={text => { this.setState({ tags: text.split(",") }) }} value={this.state.tags.toString()} placeholder="tag1 tag2 tag3" />
+
+                    <TouchableOpacity
+                        style={styles.btn}
+                        onPress={() => {
+                            route.params.callback({
+                                name: this.state.name,
+                                price: this.state.price,
+                                category: this.state.category,
+                                tags: this.state.tags
+                            })
+                        }}>
+                        <Text>+ Salvar</Text>
+                    </TouchableOpacity>
+                </View>
+            </>
         )
     }
 }
 
 const styles = StyleSheet.create({
+    header: {
+        padding: 20,
+        backgroundColor: "#fff",
+        alignItems: 'center'
+    },
+    title: {
+        fontSize: 26
+    },
+    container: {
+        flex: 1,
+        padding: 10,
+    },
     input: {
-        borderWidth: 1
+        borderWidth: 1,
+        marginBottom: 15
     },
     btn: {
         backgroundColor: '#aaa',
--- trackmobApp/src/pages/Home/index.js
+++ trackmobApp/src/pages/Home/index.js
@@ -1,11 +1,18 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+
 
 class Home extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            products: false
+            products: [{
+                name: "product 1",
+                price: "U$ 50.01",
+                category: "category1",
+                tags: ["tag1", "tag2"]
+            }]
         }
 
         this.handleClick = this.handleClick.bind(this);
@@ -20,8 +27,12 @@ class Home extends React.Component {
         const { navigation } = this.props;
 
         return (
-            <View style={styles.container}>
-                <View>
+            <>
+                <View style={styles.header}>
+                    <Text style={styles.title}>Trackmob Frontend Test</Text>
+                    <Text>My Products</Text>
+                </View>
+                <View style={styles.container}>
                     <TouchableOpacity
                         style={styles.btn}
                         onPress={() =>
@@ -30,16 +41,28 @@ class Home extends React.Component {
                         <Text>+ Cadastrar</Text>
                     </TouchableOpacity>
                     <View>
-                        
+
                         {
                             this.state.products
-                                ? this.state.products.map(response => {
+                                ? this.state.products.map((response, i) => {
                                     return (
-                                        <View style={styles.containerProd}>
-                                            <Text>{response.nome}</Text>
-                                            <Text>{response.valor}</Text>
-                                            <Text>{response.categoria}</Text>
-                                            <Text>Tags</Text>
+                                        <View style={styles.containerProd} key={i}>
+                                            <View>
+                                                <Image
+                                                    style={styles.tinyLogo}
+                                                    source={require("./../../util/notitle.png")}
+                                                />
+                                            </View>
+                                            <View>
+                                                <Text>{response.name}</Text>
+                                                <Text>{response.price}</Text>
+                                                <Text>{response.category}</Text>
+                                                <Text>{ response.tags.map(x => {return ("#"+x)}) }</Text>
+                                            </View>
+                                            <View style={{flexDirection: 'row'}}>
+                                                <Icon.Button name="trash-alt" size={15} color="#454545" backgroundColor="none" />
+                                                <Icon.Button name="edit" size={15} color="#454545" backgroundColor="none" />
+                                            </View>
                                         </View>
                                     )
                                 })
@@ -47,14 +70,28 @@ class Home extends React.Component {
                         }
                     </View>
                 </View>
-            </View>
+            </>
         )
     };
 }
 
 const styles = StyleSheet.create({
+    header: {
+        padding: 20,
+        backgroundColor: "#fff",
+        alignItems: 'center'
+    },
+    title: {
+        fontSize: 26
+    },
     container: {
         flex: 1,
+        padding: 10
+    },
+    containerProd: {
+        marginTop: 15,
+        flexDirection: 'row',
+        borderWidth: 1
     },
     btn: {
         backgroundColor: '#aaa',
